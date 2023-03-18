e4df9618d6368f040b86fdbba620b7f03d1bbab9
masuda3969 <112703629+masuda3969@users.noreply.github.com>
2023-03-18 02:42:50 +0000

    create_admin_account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
-  devise_for :publics
+  
+  #管理者側
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+     sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+  end
+  
+  #会員側
+  devise_for :users,skip: [:passwords], controllers: {
+      sessions: 'public/sessions',
+      registrations: 'public/registrations',
+  }
+  
+  scope module: :public do
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
