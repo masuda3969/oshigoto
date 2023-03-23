778a6db98a2ff1e594f1c9eb962c1227c4cf0128
masuda3969 <112703629+masuda3969@users.noreply.github.com>
2023-03-23 04:13:16 +0000

    add_authority

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # トップページとアバウトページのみログイン状態に関わらず閲覧可能
   # その他のページはログイン必須
-  # before_action :authenticate_user!, if: :use_auth?
+  before_action :authenticate_user!, except: [:top, :about]
 
   # deviseコントローラにストロングパラメータを追加
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -14,12 +14,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
-  
-  # def use_auth?
-  #   if controller_name == 'homes' && action_name == "top"
-  #     true
-      
-  
+
+
 
 
   protected
